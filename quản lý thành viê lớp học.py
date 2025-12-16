c1e23dc226e026e35a062cc6620a0259c3e0f635
yngan1226 <ngankontum12@example.com>
2025-12-16 07:32:26 +0700

    quản lý thành viên lớp học

---
+++ quản lý thành viê lớp học.py
@@ -0,0 +1,52 @@
+class SinhVien:
+    def __init__(self, ma_sv, ho_ten, email):
+        self.ma_sv = ma_sv
+        self.ho_ten = ho_ten
+        self.email = email
+
+    def hien_thi(self):
+        print("Mã SV:", self.ma_sv)
+        print("Họ tên:", self.ho_ten)
+        print("Email:", self.email)
+ class LopHoc:
+    def __init__(self, ten_lop, giang_vien):
+        self.ten_lop = ten_lop
+        self.giang_vien = giang_vien
+        self.ds_sinh_vien = []
+ ##hiện thị sinh viên:   
+    def hien_thi_ds(self):
+        if len(self.ds_sinh_vien) == 0:
+            print("Lớp chưa có sinh viên.")
+            return
+
+        print(f"Danh sách sinh viên lớp {self.ten_lop}:")
+        for sv in self.ds_sinh_vien:
+            print(f"- {sv.ma_sv} | {sv.ho_ten}")
+##xem thông tin sinh viên
+    def tim_sv(self, ma_sv):    
+        for sv in self.ds_sinh_vien:
+            if sv.ma_sv == ma_sv:
+                sv.hien_thi()
+                return
+        print("Không tìm thấy sinh viên.")
+##thêm sinh viên
+        def them_sv(self, sv, nguoi_dung):
+        if nguoi_dung != self.giang_vien:
+            print("❌ Không có quyền thêm sinh viên.")
+            return
+
+        self.ds_sinh_vien.append(sv)
+        print("✅ Thêm sinh viên thành công.")
+##xóa sinh viên
+        def xoa_sv(self, ma_sv, nguoi_dung):
+        if nguoi_dung != self.giang_vien:
+            print("❌ Không có quyền xóa sinh viên.")
+            return
+
+        for sv in self.ds_sinh_vien:
+            if sv.ma_sv == ma_sv:
+                self.ds_sinh_vien.remove(sv)
+                print("✅ Đã xóa sinh viên.")
+                return
+
+        print("Không tìm thấy sinh viên.")
